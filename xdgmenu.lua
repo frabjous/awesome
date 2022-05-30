e681d88cd2539d77f9df116647176660ca5918a5
Kevin Klement <klement@umass.edu>
2022-05-30 12:18:43 -0400

    kckupdate push 2022/05/30 12:18:35

--- xdgmenu.lua
+++ xdgmenu.lua
@@ -1,10 +1,8 @@
  local menu98edb85b00d9527ad5acebe451b3fae6 = {
      {"Character Map", "gucharmap", "/usr/share/icons/gnome/16x16/apps/accessories-character-map.png" },
-     {"Document Viewer", "xreader ", "/usr/share/icons/hicolor/16x16/apps/xreader.png" },
-     {"HP Device Manager", "hp-toolbox", "///usr/share/hplip/data/images/128x128/hp_logo.png" },
      {"Neovim", "xterm -e nvim ", "/usr/share/icons/hicolor/128x128/apps/nvim.png" },
-     {"Winetricks", "winetricks --gui"},
-     {"XCA", "xca ", "/usr/share/icons/hicolor/16x16/apps/xca.png" },
+     {"Passwords and Keys", "seahorse "},
+     {"Xfburn", "xfburn"},
      {"compton", "compton", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
      {"picom", "picom"},
  }
@@ -14,8 +12,7 @@
  }
 
  local menu251bd8143891238ecedc306508e29017 = {
-     {"SuperTux 2", "supertux2"},
-     {"SuperTuxKart", "supertuxkart", "/usr/share/icons/hicolor/16x16/apps/supertuxkart.png" },
+     {"Extreme Tux Racer", "etr", "/usr/share/pixmaps/etr.png" },
  }
 
  local menud334dfcea59127bedfcdbe0a3ee7f494 = {
@@ -29,7 +26,7 @@
      {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
      {"Okular", "okular ", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"XDvi", "xdvi "},
-     {"XSane - Scanning", "xsane", "/usr/share/pixmaps/xsane.xpm" },
+     {"llpp", "llpp "},
  }
 
  local menuc8205c7636e728d448c2774e6a4a944b = {
@@ -37,13 +34,13 @@
      {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
      {"Brave", "brave ", "/usr/share/icons/hicolor/16x16/apps/brave-desktop.png" },
      {"Chromium", "/usr/bin/chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"ELinks", "xterm -e /usr/bin/elinks ", "/usr/share/icons/gnome/16x16/mimetypes/html.png" },
      {"Falkon", "falkon ", "/usr/share/icons/hicolor/16x16/apps/falkon.png" },
      {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
-     {"qutebrowser", "qutebrowser --untrusted-args ", "/usr/share/icons/hicolor/16x16/apps/qutebrowser.png" },
+     {"Transmission", "transmission-gtk ", "/usr/share/pixmaps/transmission.png" },
  }
 
  local menudf814135652a5a308fea15bff37ea284 = {
-     {"Atril Document Viewer", "atril ", "/usr/share/icons/hicolor/16x16/apps/atril.png" },
      {"Calibre", "calibre --detach ", "/usr/share/icons/hicolor/16x16/apps/calibre-gui.png" },
      {"Document Viewer", "evince "},
      {"E-book editor", "ebook-edit --detach ", "/usr/share/icons/hicolor/16x16/apps/calibre-ebook-edit.png" },
@@ -57,24 +54,20 @@
      {"LibreOffice Impress", "libreoffice --impress ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-impress.png" },
      {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
      {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+     {"LyX", "lyx ", "/usr/share/icons/hicolor/48x48/apps/lyx.png" },
      {"Okular", "okular ", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
      {"TeXstudio", "texstudio ", "/usr/share/icons/hicolor/16x16/apps/texstudio.png" },
      {"TeXworks", "texworks ", "/usr/share/icons/hicolor/48x48/apps/TeXworks.png" },
      {"Texmaker", "texmaker ", "/usr/share/pixmaps/texmaker.png" },
      {"Xournal++", "xournalpp "},
      {"Zathura", "zathura ", "/usr/share/icons/hicolor/16x16/apps/org.pwmt.zathura.png" },
-     {"apvlv", "apvlv "},
+     {"llpp", "llpp "},
      {"qpdfview", "qpdfview --unique "},
  }
 
  local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
      {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
-     {"Qt Assistant", "assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
-     {"Qt Designer", "designer ", "/usr/share/icons/hicolor/128x128/apps/QtProject-designer.png" },
-     {"Qt Linguist", "linguist ", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
-     {"Qt QDBusViewer ", "qdbusviewer", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer.png" },
      {"Sioyek", "sioyek", "/usr/share/icons/sioyek-icon-linux.png" },
-     {"UserFeedback Console", "UserFeedbackConsole", "/usr/share/icons/gnome/16x16/actions/search.png" },
  }
 
  local menu80d754c83abbbd4ef5d150904efcc86c = {
@@ -82,27 +75,29 @@
  }
 
  local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Audacity", "env UBUNTU_MENUPROXY=0 audacity ", "/usr/share/icons/hicolor/16x16/apps/audacity.png" },
+     {"Cheese", "cheese"},
      {"DevedeNG", "devede_ng.py"},
-     {"HandBrake", "ghb "},
      {"Kdenlive", "kdenlive ", "/usr/share/icons/hicolor/16x16/apps/kdenlive.png" },
-     {"MakeMKV", "makemkv", "/usr/share/icons/hicolor/16x16/apps/makemkv.png" },
+     {"MKVToolNix GUI", "mkvtoolnix-gui ", "/usr/share/icons/hicolor/16x16/apps/mkvtoolnix-gui.png" },
      {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
      {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
      {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
      {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"Xfburn", "xfburn"},
      {"mpv Media Player", "mpv --player-operation-mode=pseudo-gui -- ", "/usr/share/icons/hicolor/16x16/apps/mpv.png" },
  }
 
  local menuee69799670a33f75d45c57d1d1cd0ab3 = {
      {"Alacritty", "alacritty", "/usr/share/pixmaps/Alacritty.svg" },
      {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
-     {"Dolphin", "dolphin ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
+     {"Bulk Rename", "thunar --bulk-rename ", "/usr/share/icons/hicolor/16x16/apps/org.xfce.thunar.png" },
      {"GParted", "/usr/bin/gparted ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
      {"Hardware Locality lstopo", "lstopo"},
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
      {"Manage Printing", "/usr/bin/xdg-open http://localhost:631/", "/usr/share/icons/hicolor/16x16/apps/cups.png" },
+     {"Thunar File Manager", "thunar ", "/usr/share/icons/hicolor/16x16/apps/org.xfce.thunar.png" },
      {"Tint2", "tint2"},
-     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
-     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
      {"dconf Editor", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/ca.desrt.dconf-editor.png" },
      {"jgmenu", "jgmenu_run"},
      {"kitty", "kitty", "/usr/share/icons/hicolor/256x256/apps/kitty.png" },
