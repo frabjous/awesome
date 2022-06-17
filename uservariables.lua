50b4ffe8bad10a88f26a773aa5d00e54b66bde7d
Kevin Klement <klement@umass.edu>
2022-06-17 11:57:48 -0400

    kckupdate push 2022/06/17 11:57:39

--- uservariables.lua
+++ uservariables.lua
@@ -4,7 +4,7 @@ local _M = {
     terminal   = os.getenv("TERMINAL") or "wezterm",
     editor     = os.getenv("EDITOR") or "nvim",
     browser    = os.getenv("BROWSER") or "firefox",
-    editor_cmd = "wezterm -- nvim",
+    editor_cmd = "wezterm start -- nvim",
     modkey     = "Mod4",
     altkey     = "Mod1"
 }
