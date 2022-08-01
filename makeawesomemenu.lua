057bf5740dc1a3b613609eaf04b2df23686d8d56
Kevin Klement <klement@umass.edu>
2022-07-31 22:49:09 -0400

    kron push 2022-07-31 22:49:09

--- makeawesomemenu.lua
+++ makeawesomemenu.lua
@@ -2,6 +2,8 @@
 
 require('pl')
 
+local parsed = {}
+
 local menus = {
     Accessories = {},
     Education = {},
@@ -18,7 +20,45 @@ local desktop_folders = {
     '/usr/share/applications'
 }
 
+local importantkeys = {
+    'Categories',
+    'Name',
+    'Exec',
+    'Terminal',
+    'Icon',
+    'NoDisplay'
+}
+----------------------------------------
+-- FUNCTIONS
+----------------------------------------
+
+function handle_desktopfile(desktopfile)
+    local props = {}
+    local f = io.open(desktopfile)
+    for line in f:lines() do
+        if (line:match('=')) then
+            local linesplit = stringx.split(line,'=',2)
+            linekey, lineval = linesplit[1], linesplit[2]
+            if ((props[linekey] == nil) and (tablex.find(importantkeys, linekey))) then
+                props[linekey] = lineval
+            end
+        end
+    end
+    f:close()
+    pretty.dump(props)
+end
+
+
+----------------------------------------
+-- ROUTINES
+----------------------------------------
 for i, folder in ipairs(desktop_folders) do
     local all_desktops = dir.getallfiles(folder, '*.desktop')
-    pretty.dump(all_desktops)
+    for j, desktopfile in ipairs(all_desktops) do
+        basename = path.basename(desktopfile)
+        if (tablex.find(parsed, basename) == nil) then
+            handle_desktopfile(desktopfile)
+            table.insert(parsed, basename)
+        end
+    end
 end
