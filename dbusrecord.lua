dd698424ed42563752796e65fea3eece5542677b
Kevin Klement <klement@umass.edu>
2022-09-18 02:37:06 -0400

    kron push 2022-09-18 02:37:06

--- dbusrecord.lua
+++
@@ -1,6 +0,0 @@
-
-local dbusaddress=os.getenv("DBUS_SESSION_BUS_ADDRESS")
-local recordfile=os.getenv("XDG_RUNTIME_DIR") .. "/dbus.addr"
-local f=io.open(recordfile, "w")
-f:write(dbusaddress)
-f:close()
