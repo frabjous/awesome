cdcac2ab1b5525242731ee91b724732a3da7b5e8
Kevin Klement <klement@umass.edu>
2023-06-24 10:13:05 -0400

    kron push 2023-06-24 10:13:05

--- statusbar.lua
+++ statusbar.lua
@@ -1221,6 +1221,8 @@ awful.screen.connect_for_each_screen(function(s)
             arleft,
             clipboardw,
             spacey,
+            readitw,
+            spacey,
             scratchw,
             spacey,
             quakew,
